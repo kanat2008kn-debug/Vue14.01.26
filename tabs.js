a4f040b3c5fbd762db108ec2e2b64332adea40b5
Kanat <kanat2008.kn@gmail.com>
2026-01-19 00:04:22 +0500

    задание Табы

---
+++ tabs.js
@@ -0,0 +1,26 @@
+const app = Vue.createApp({
+    data: function() {
+        return {
+            currentTab: 2,
+            items: [
+                { 
+                    title: 'Вкладка1',
+                    text: '111POIPOIPOIPOIPO5s4df65s4df65sd4f65s4df65s4d6f54sdfsdfsdf5s65dfsdfsdfsdfsdf564seterte6r5t4er6t5er6t54e6r5t4er65t4e6r54tdfsdfIUIUYIUYIUYIUYIdf32s1df321s32f1d32sf1d3sdf13sdf13s21df3s2f1dIUIUYIUYIUYIUYIdf32s1df321s32f1d32sf1d3sdf13sdf13s21df3s2f1d',
+                },
+                { 
+                    title: 'Вкладка2',
+                    text: '222IUIUYIUYIUYIUYIdf32s1df321s32f1d32sf1d3sdf13sdf13s21df3s2f1dIUIUYIUYIUYIUYIdf32s1df321s32f1d32sf1d3sdf13sdf13s21df3s2f1dIUIUYIUYIUYIUYIdf32s1df321s32f1d32sf1d3sdf13sdf13s21df3s2f1d',
+                },
+                { 
+                    title: 'Вкладка3',
+                    text: '333GFDGFDGFDGFDGDGsdfsdfsdf32s1df321s32f1d32sf1d3sdf13sdf13s21df3s2f1dIUIUYIUYIUYIUYIdf32s1df321s32f1d32sf1d3sdf13sdf13s21df3s2f1dIUIUYIUYIUYIUYIdf32s1df321s32f1d32sf1d3sdf13sdf13s21df3s2f1d',
+                },
+            ]
+        };
+    },
+    methods: {
+        
+    }
+});
+
+app.mount('#app');
